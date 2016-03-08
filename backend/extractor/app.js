f67a3b89346df233df525004159d5647f390e7d3
mcg25 <mirco.chiang@gmail.com>
2016-03-08 19:19:18 +0000

    Initial Setup

---
+++ backend/extractor/app.js
@@ -0,0 +1,11 @@
+var nmailer = require('nodemailer');
+var awssdk = require('aws-sdk');
+var nschedule = require('node-schedule');
+var request = require('request');
+
+//Quandl API Key
+var qApiKey = ZqWstC3utJ_y5ij3gExN;
+//NOAA API Token
+var wToken = wxAttCfTwYnvVmjKlRyFuMcmoymfuxKi;
+
+
